6917b13bca54cd449b7e2da201cdbaae873f07db
Alex <alex.gromov34502@gmail.com>
2024-08-02 18:48:11 +0300

    Порт вынесен в .env

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ config();
 const app = express();
 app.use(json());
 
-app.listen(3001, () => {
-	console.log(`Server started at port: ${3001}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`Server started at port: ${PORT}`);
 });
